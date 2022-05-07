a3e2bc419d0ff549f75a9a889b70f56db14f26d8
kokikudo <massanmesu@icloud.com>
2022-05-07 14:59:32 +0900

    初コミット。プレイグラウンド作成

---
+++ RxSwift_lesson_with_Udemy/PlayGround/MyPlayground.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+import RxSwift
+import RxCocoa
+
+let dispose = DisposeBag()
+
+let behaviorSubject = BehaviorSubject(value: "init value")
+
+behaviorSubject.onNext("last value")
+
+behaviorSubject.subscribe{
+    print($0)
+}.disposed(by: dispose)
+
+behaviorSubject.onNext("next value")
+
+print("--ReplaySubject--")
+let replaySub = ReplaySubject<String>.create(bufferSize: 2)
+
+replaySub.onNext("value 1")
+replaySub.onNext("value 2")
+replaySub.onNext("value 3")
+
+replaySub.subscribe { print($0) }.disposed(by: dispose)
+
+replaySub.onNext("value 4")
+replaySub.onNext("value 5")
+replaySub.onNext("value 6")
+
+print("--ReplaySUbject2--")
+replaySub.subscribe { print($0) }.disposed(by: dispose)
+
+print("--BehaviorRelay--")
+let behaviorReplay = BehaviorRelay(value: "init value")
+behaviorReplay.subscribe { print($0) }.disposed(by: dispose)
+behaviorReplay.accept("next Value")
+// 配列に要素を追加
+let behaviorArray = BehaviorRelay(value: ["item 1"])
+behaviorArray.subscribe { print($0) }.disposed(by: dispose)
+var newArray = behaviorArray.value
+newArray.append("item 2")
+newArray.append("item 3")
+behaviorArray.accept(newArray)
